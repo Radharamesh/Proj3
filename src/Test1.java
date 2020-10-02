d72ae2f8acfc6e4a0086415ff6b297bd52c1ff09
Radharamesh <72200094+Radharamesh@users.noreply.github.com>
2020-10-02 22:02:45 +0200

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,9 @@
 public class Test1 {
     public void print() {
         System.out.println("second class");
+        System.out.println("changes made in git hub");
+        
+           
     }
 
 }
